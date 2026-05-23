fae5af9b80e629caa9d9c46c58e3e66e953fcd8e
renan.cavalcanti <renan.cavalcanti@kurier.com.br>
2026-05-23 09:53:35 -0300

    feat: add dotenv support and .env.example

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const { globalLimiter, suggestLimiter, dailyQuotaMiddleware } = require('./middleware/rateLimit.middleware');
 const { sanitizePrompt } = require('./utils/sanitizer');
